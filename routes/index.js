b58648c987fb0dd8d49178be9d079e77575d405a
kiss8981 <kdh89811@gmail.com>
2021-05-19 21:01:47 +0900

    메인페이지 봇 정보 표시 추가

--- routes/index.js
+++ routes/index.js
@@ -1,11 +1,25 @@
 const express = require('express');
 const router = express.Router();
 const settings = require("../settings.json");
+const Discord = require("discord.js");
+const client = new Discord.Client();
+
+client.on('ready', () => {
+  console.log(`Logged in as ${client.user.tag}!`);
+});
+client.login("ODE1NDg0NzQ3MTQwMDM4NjY2.YDtFWg.y0shzrv8kLJke9_h9_zrJGIKQYA")
 
 /* GET home page. */
+function numberWithCommas(x) {
+    return x.toString().replace(/\B(?=(\d{3})+(?!\d))/g, ",");
+}
 
 router.get('/', function(req, res) {
-  res.render('../views/index', { bot: settings.website });
+  res.render('../views/index', {
+    bot: settings.website,
+    member: numberWithCommas(client.users.cache.size),
+    guilds: numberWithCommas(client.guilds.cache.size)
+  });
 });
 router.get('/commands', (req, res) => {
   res.render('../views/commands', {bot: settings.website, commands: settings.commands });
